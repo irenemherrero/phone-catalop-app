ab5871109fef3794b6527a09fe97e54b629f927d
irenemherrero <irenemherrero@gmail.com>
2018-11-02 02:02:43 +0100

    preparing deploy

---
+++ docs/precache-manifest.781a29983f56a196891e2dd725c21ead.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cffa9d9d88890cfcd0ae3d6dd7a6efa9",
+    "url": "./static/media/spinner.cffa9d9d.gif"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "066b4d8b4036363654bc",
+    "url": "./static/js/main.066b4d8b.chunk.js"
+  },
+  {
+    "revision": "1e3ba0ee7abfb46b6a01",
+    "url": "./static/js/1.1e3ba0ee.chunk.js"
+  },
+  {
+    "revision": "066b4d8b4036363654bc",
+    "url": "./static/css/main.fdc88d15.chunk.css"
+  },
+  {
+    "revision": "3a506d927e0ec2af31afd100296dc8f3",
+    "url": "./index.html"
+  }
+];
--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://my-json-server.typicode.com/irenemherrero/demo/devices/')
+    fetch('http://my-json-server.typicode.com/irenemherrero/demo/devices/')
       .then(response => {
         return response.json();
       })
